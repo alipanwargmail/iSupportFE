f7916c75bd12c40c97bf46064c472bf9749330a6
alipanwargmail <alipanwar@gmail.com>
2023-10-14 11:11:25 +0700

    A

--- src/TestChart.js
+++ src/TestChart.js
@@ -14,11 +14,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Avatar from '@material-ui/core/Avatar';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import { useEffect } from 'react';
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -70,9 +67,9 @@ export default function TestChart() {
       }
     }
 
-    var sdata = JSON.stringify(objects, null, 2);
+    //var sdata = JSON.stringify(objects, null, 2);
     setChartData(objects);
-    console.log(chartData);
+    //console.log(chartData);
   }, [])
   const handleClose = () => {
     setAnchorEl(null);
